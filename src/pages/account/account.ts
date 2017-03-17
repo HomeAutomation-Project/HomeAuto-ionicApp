294537a8f2bb95140d0f108edbc508e23ac24a78
Aman Vishnani <aman62@gmail.com>
2017-03-17 10:46:24 +0530

    added account updates

--- src/pages/account/account.ts
+++ src/pages/account/account.ts
@@ -7,10 +7,21 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'account.html'
 })
 export class Account {
-
+  public user: any={};
   constructor(public navCtrl: NavController, private acc:AccountService) {
-        this.acc.getAccountDetails().subscribe(res=>{console.log(res)});
+        this.user.username ="";
+        this.user.first="";
+        this.user.last="";
+        this.user.email="";
+        this.acc.getAccountDetails().subscribe(res=>{
+          console.log(res);
+          this.user.first = res.name.first;
+          this.user.last = res.name.last;
+          this.user.username = res.username;
+          this.user.email =res.email;
+        });
           
   }
+  
 
 }
