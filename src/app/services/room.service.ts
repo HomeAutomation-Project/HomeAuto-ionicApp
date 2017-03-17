6b0bbbbf0d33c9c7c5f09680472bee9213ca3c9b
darshan sarje <dds.sarje@gmail.com>
2017-03-17 12:37:01 +0530

    added room service

---
+++ src/app/services/room.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Http,Response,Headers,RequestOptions } from '@angular/http';
+import { LoginService } from '../services/login.service';
+@Injectable()
+export class RoomService{
+    baseUrl: string;
+    myUrl: string;
+    constructor (private ls:LoginService, private http:Http)
+    {
+        this.myUrl = localStorage.getItem('url') || 'auto.amanvishnani.com/api';
+        this.baseUrl = localStorage.getItem('protocol') =='0' ? 'http://' : 'https://';
+        this.baseUrl= this.baseUrl+this.myUrl;
+        console.log(this.baseUrl);
+    }
+    getaAllRooms(loc:any){
+        let token = this.ls.getToken();
+        let headers = new Headers({'Content-Type':'application/json','x-access-token':token});
+        let options = new RequestOptions({'headers':headers});
+        return this.http.get(this.baseUrl+'/room/'+loc,options)
+                    .map((res:Response) => res.json())
+                    .catch((error:any) => Observable.throw(error.json().error || 'Server error'));
+    }
+}
