8de12a0c266ea09d6655dfe16f9229c5633da547
darshan <dds.sarje@gmail.com>
2017-03-23 15:06:49 +0530

    few changes

--- src/pages/switch/switch.ts
+++ src/pages/switch/switch.ts
@@ -15,16 +15,33 @@ import { SwitchService } from '../../app/services/switch.service';
 export class SwitchPage {
   public roomDetails = this.navParams.data;
   public switches:any;
+  public states = [];
   constructor(public navCtrl: NavController, public navParams: NavParams, private ss:SwitchService) {}
 
   ionViewDidLoad() {
     //console.log('ionViewDidLoad SwitchPage');
     this.ss.getAllSwitches(this.roomDetails.location,this.roomDetails.room).subscribe(res=>{
       this.switches=res;
+      for(let i=0; i<res.length; i++)
+        this.states[i]=this.getStatus(res[i].status);
       console.log(res);
     });
     //console.log(this.roomDetails);
-    
   }
+
+  
+    getStatus(x)
+    {
+      console.log(x)
+      if(x == 'ON')
+        {
+          console.log("ON Executed")
+          return true;
+        }
+      else
+      {
+         return false;
+      }
+    }
 
 }
